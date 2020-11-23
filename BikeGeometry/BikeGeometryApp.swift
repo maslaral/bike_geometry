faef18e1e13105f0829744132d997afd393c7598
Alex Maslar <maslaral@gmail.com>
2020-11-23 10:30:06 -0600

    Initial Commit

---
+++ BikeGeometry/BikeGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeGeometryApp.swift
+//  BikeGeometry
+//
+//  Created by Alex Maslar on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct BikeGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BikeGeometry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BikeGeometry
+//
+//  Created by Alex Maslar on 11/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
